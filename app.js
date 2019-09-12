4e23ac6721155a1f04459a52bd2efe4359fcfba5
Garrett Cone <conegs1@gmail.com>
2019-09-12 23:17:37 +0000

    Connect necessary requirements for express, body parser, and ejs

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
-var express = require("express");
-var app = express();
+var express = require("express"),
+	app = express(),
+	bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
 
 app.listen(process.env.PORT || 3000, process.env.IP, function(){
 	console.log("Natural Acai Co. server started...");
