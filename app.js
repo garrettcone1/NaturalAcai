ea5d6b61a6c47e7c2026e47a958f9ba1b4aaa3e3
Garrett Cone <conegs1@gmail.com>
2019-09-13 04:25:18 +0000

    Connect homePage.js to app.js

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ var homePageRoutes = require("./routes/homePage");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
+// Use Routes
+app.use(homePageRoutes);
+
 app.listen(process.env.PORT || 3000, process.env.IP, function(){
 	console.log("Natural Acai Co. server started...");
 });
