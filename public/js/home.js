702137cad1d7a892f5c654094fcfb8e7f5739578
Garrett Cone <conegs1@gmail.com>
2019-10-25 19:17:33 +0000

    Remove redundant code.

--- public/js/home.js
+++ public/js/home.js
@@ -17,17 +17,6 @@ const navSlider = () => {
 		// Mobile Menu Animation
 		mobileMenu.classList.toggle("toggle");
 	});
-	
-	var prevScrollpos = window.pageYOffset;
-	window.onscroll = function() {
-	  var currentScrollPos = window.pageYOffset;
-	  if (prevScrollpos > currentScrollPos) {
-		document.getElementById("nav").style.top = "0";
-	  } else {
-		document.getElementById("nav").style.top = "-50px";
-	  }
-	  prevScrollpos = currentScrollPos;
-	}
 }
 
 navSlider();
